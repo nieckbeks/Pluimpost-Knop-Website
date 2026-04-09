fe383a0e3dff66a3cbc889d8d5c9a84c2a2a4eb5
nieckbeks <nieckbeks@gmail.com>
2026-04-09 16:06:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/* De intro animatie*/
+let intro = document.querySelector('.intro');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+     intro.style.top = '-100vh';
+
+    },1500)
+});
+
+/* Inlever berichtjes */
+let ingeleverd = [
+  "Complimentje ingeleverd!",
+  "Dankjewel voor het compliment!",
+  "Je bent een topper!",
+];
+
+/* Koppelen aan de Html*/
+let button = document.getElementById("btn");
+let card = document.getElementById("card");
+
+/* Het hele knopgebeuren gedoe*/
+button.addEventListener("click", () => {
+  let random = Math.floor(Math.random() * ingeleverd.length);
+  
+  card.innerHTML = `
+    ${ingeleverd[random]}<br>
+    <button class="btn" id="btn">Klik hier</button>
+  `;
+});
